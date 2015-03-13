5db505757ef2feb8563576f63e8c98891069557c
Donovan Mikrot <donovan@donovan.mn>
2015-03-12 21:11:24 -0500

    Add script to publish to gh-pages

---
+++ publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Clean slate
+git clean -df
+git checkout source
+rm -rf public
+
+# Build deployables
+brunch build --production
+
+# Switch to production branch
+git checkout master
+
+# Clean slate
+rm -rf fonts
+rm -rf javascripts
+rm -rf static
+rm -rf stylesheets
+rm -rf favicon.ico
+rm -rf index.html
+
+# Copy deployables
+mv public/* ./
+rmdir public
+
+echo "Commit changes to publish"
