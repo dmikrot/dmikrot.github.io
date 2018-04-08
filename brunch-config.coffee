92ddc5b0c80ec61c084aa11eb5fe97f77681b9b5
Donovan Mikrot <donovan@donovan.mn>
2018-04-08 11:19:57 -0500

    Switch brunch config to JS

--- brunch-config.coffee
+++
@@ -1,9 +0,0 @@
-exports.config =
-  # See http://brunch.io/#documentation for docs.
-  files:
-    javascripts:
-      joinTo: 'javascripts/app.js'
-    stylesheets:
-      joinTo: 'stylesheets/app.css'
-    templates:
-      joinTo: 'templates/app.js'
---
+++ brunch-config.js
@@ -0,0 +1,14 @@
+// See http://brunch.io/#documentation for docs.
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: 'javascripts/app.js'
+    },
+    stylesheets: {
+      joinTo: 'stylesheets/app.css'
+    },
+    templates: {
+      joinTo: 'templates/app.js'
+    }
+  }
+}
